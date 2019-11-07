901fb8557cb778f50cf2701ad93fdc6e5d950b08
Eli Hood <eli.hood@aol.com>
2019-11-07 15:37:48 -0600

    update

--- client/src/Axios.tsx
+++ client/src/Axios.tsx
@@ -2,20 +2,20 @@ import Axios from "axios";
 import "dotenv/config";
 
 const AxiosInstance = Axios.create({
-  baseURL: "http://localhost:3000", // localhost:3000
+  baseURL: process.env.REACT_APP_BASE_URL, // localhost:3000
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
+    Accept: "application/json"
+  }
 });
 
 AxiosInstance.interceptors.response.use(
-  (response) =>
+  response =>
     // const token = localStorage.getItem('auth');
     // response.headers.Authorization =  token ? `Bearer ${token}` : '';
     // console.log(token);
-    response,
+    response
 );
 
 export default AxiosInstance;
