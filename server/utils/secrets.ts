b386ebc59695d2d1c029a1ead26a2269a4a0f1b4
elli <eli@ellis-MacBook-Pro.local>
2020-01-19 13:27:52 -0600

    removeUtils

--- server/utils/secrets.ts
+++
@@ -1,6 +0,0 @@
-import * as dotenv from "dotenv";
-
-export const { PSQL_NAME } = process.env;
-export const { PSQL_USER } = process.env;
-export const { PSQL_PASS } = process.env;
-export const { PSQL_HOST } = process.env;
