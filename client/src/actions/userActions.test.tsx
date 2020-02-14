5259d67e7bf0e3ef061bb7ac56b4732fda1cacb0
elli <eli@ellis-mbp.attlocal.net>
2020-02-13 19:09:56 -0600

    userActions

---
+++ client/src/actions/userActions.test.tsx
@@ -0,0 +1,43 @@
+import * as actionTypes from "../actions/userActions";
+import * as types from "../actionTypes/userActionTypes";
+
+describe("userActions", () => {
+    it("should test signup action", () => {
+        const payload = {
+            email: "test@example.com",
+            username: "JohnWayne",
+            password: "123fish",
+        };
+
+        const expectedAction = {
+            type: types.SIGN_UP_INIT,
+            payload: payload,
+            history: {},
+        };
+
+        expect(actionTypes.signUpInit(payload, {})).toEqual(expectedAction);
+    });
+    it("should test loginInit action", () => {
+        const payload = {
+            username: "JohnWayne",
+            password: "123fish",
+        };
+        const expectedAction = {
+            type: types.LOG_IN_INIT,
+            payload: payload,
+            history: {},
+        };
+        expect(actionTypes.loginInit(payload, {})).toEqual(expectedAction);
+    });
+
+    it("should test loginSuccess action", () => {
+        const payload = {};
+
+        const expectedAction = {
+            type: types.LOG_IN_SUCCESS,
+            payload,
+        };
+
+        expect(actionTypes.loginSuccess(payload)).toEqual(expectedAction);
+    });
+});
