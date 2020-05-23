2a6b5d1ead25fabbd15fe64cf7fe1052b12d5a74
elihood <eli@elihoods-MacBook-Pro.local>
2020-05-23 19:29:03 -0400

    update

---
+++ client/src/components/forms/commentAuthorData/commentAuthorData.test.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import toJson from "enzyme-to-json";
+import CommentAuthorData from "./commentAuthorData";
+import { shallow, mount, render } from "enzyme";
+import storeHooks from "../../../common/storeHooks";
+import { Provider } from "react-redux";
+import { store } from "../../../store";
+
+describe("Should test CommentAuthorData", () => {
+    let wrapper;
+    const props = {
+        comment: {
+            author: {
+                gravatar: "https://hype.my/wp-content/uploads/2015/02/SpongeBob-SquarePants-Fun-Facts.jpg",
+            },
+        },
+        currentUser: {},
+        isBold: jest.fn(),
+    };
+    beforeAll(() => {
+        wrapper = mount(
+            <Provider store={store}>
+                <CommentAuthorData {...props} />
+            </Provider>,
+        );
+    });
+    it("should test <CommentAuthorData/>", () => {
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it("should check <img>", () => {
+        expect(wrapper.find("img").at(0)).toHaveLength(1);
+    });
+
+    it("should render <CommentAuthorData/>", () => {
+        expect(wrapper).toHaveLength(1);
+    });
+});
