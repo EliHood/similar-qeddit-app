b1b057e340d790069af9fa6a6e9de231d8144b27
elihood <eli@elihoods-MacBook-Pro.local>
2020-07-30 13:47:51 -0400

    update

--- client/src/components/forms/commentListContainer/commentListContainer.tsx
+++ client/src/components/forms/commentListContainer/commentListContainer.tsx
@@ -24,7 +24,7 @@ const CommentListContainer: RefForwardingComponent<HTMLDivElement, any> = (props
         };
 
         replyComm(data);
-        // setReplyComment(false);
+        setReplyComment(false);
         setReply("");
     };
     return (
