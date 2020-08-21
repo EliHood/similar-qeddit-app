0472e76c9f998ec7678138072c84d9ffe1dccea6
elihood <eli@elihoods-MacBook-Pro.local>
2020-08-21 11:37:19 -0400

    update

--- client/src/components/search/Search.tsx
+++ client/src/components/search/Search.tsx
@@ -17,12 +17,21 @@ const Search: React.FC<SearchType> = (props) => {
         e.preventDefault();
         history.push({ pathname: `/search/posts?q=${query}`, state: { query: query, currentUser: props.currentUser } });
     };
-
+    const disableIfEmpty = query.length === 0 ? true : false;
+    const ifTyping = query.length > 0 ? "100px" : "-40px";
+    const fadeIfempty = query.length > 0 ? 1 : 0.1;
     return (
         <Fragment>
             <form onSubmit={onSubmit}>
                 <InputBase placeholder="Search…" style={{ color: "#fff" }} onChange={(e) => search(e.target.value)} value={query} inputProps={{ "aria-label": "search" }} />
-                <Button style={{ marginLeft: "80px", backgroundColor: "transparent" }} size="small" type="submit" variant="outlined" color="primary">
+                <Button
+                    disabled={disableIfEmpty}
+                    style={{ marginLeft: ifTyping, opacity: fadeIfempty, transition: "ease 0.4s", outline: "none", backgroundColor: "transparent" }}
+                    size="small"
+                    type="submit"
+                    variant="outlined"
+                    color="primary"
+                >
                     <SearchIcon style={{ color: "#fff" }} />
                 </Button>
             </form>
