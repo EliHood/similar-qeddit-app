e45997e102ac279b0eae3315133e02261ea73978
elihood <eli@elihoods-MacBook-Pro.local>
2021-04-09 21:38:13 -0400

    fix heroku issues

--- server/config/database.config.ts
+++ server/config/database.config.ts
@@ -15,6 +15,7 @@ export interface ProductionDbConfig {
   host: string;
   dialect: string;
   use_env_variable: string;
+  ssl:boolean;
 }
 
 export const productionConfig: ProductionDbConfig = {
@@ -24,7 +25,9 @@ export const productionConfig: ProductionDbConfig = {
     "postgres://hdojgllysojlff:b95812dc8d1159b0c0243a1bfe8298ff49b07976fe81ee8165766d6b9f52a70c@ec2-54-167-168-52.compute-1.amazonaws.com:5432/d1eqsee9ljmf24",
   host: "127.0.0.1",
   dialect: "postgres",
+  ssl: true,
   use_env_variable: "DATABASE_URL",
+  
 };
 
 export const databaseConfig: DatabaseConfig = {
