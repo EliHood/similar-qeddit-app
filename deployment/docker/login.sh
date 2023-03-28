162093ea51c19d1f1d1c0c761965a8969bb55c6d
visortelle <visortelle@gmail.com>
2023-03-28 19:00:52 +0200

    Check variables exist on CI

--- deployment/docker/login.sh
+++ deployment/docker/login.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 
 echo "Login to Docker registry"
 
+printf $DOCKER_PASSWORD | wc -c
+printf $DOCKER_USERNAME | wc -c
+
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
